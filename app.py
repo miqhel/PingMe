392e196aeac7d722474e92c08900d0038194e81c
miqhel <michaelolarotimi89@gmail.com>
2025-11-21 23:54:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -389,5 +389,7 @@ def uploaded_file(filename):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False)    
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port
+    app.run(host="0.0.0.0", port=port)    
+
 
